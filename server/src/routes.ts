9cfb7d860f6b9ff4331dddb4e84607612696cf74
lucca <lucca.gdias07@gmail.com>
2020-08-08 02:21:08 -0300

    ConnectionsController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,6 +9,7 @@ const connectionsController = new ConnectionController();
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
 
+routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
 
 export default routes;
