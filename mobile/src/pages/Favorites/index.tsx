fc91cfac662b1b419bfbed57964bf601aa8d2dd3
lucca <lucca.gdias07@gmail.com>
2020-08-09 20:10:17 -0300

    Mobile - TeacherItem

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -2,12 +2,32 @@ import React from  'react';
 import { View } from 'react-native';
 
 import styles from './styles';
+import { ScrollView } from 'react-native-gesture-handler';
+
+
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 function Favorites() {
     return (
         <View style={styles.container}>
             <PageHeader title="Meus proffys favoritos"/>
+
+            <ScrollView 
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16
+                }}
+            >
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+
+            </ScrollView>
         </View>
     );
 }
