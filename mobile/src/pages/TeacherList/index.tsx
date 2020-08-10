b357f7ebdeee00094551d4233160fc1725de217f
lucca <lucca.gdias07@gmail.com>
2020-08-09 21:00:44 -0300

    Mobile - TeacherList

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,16 +1,90 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons'
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import styles from './styles'
-import TeacherItem from '../../components/TeacherItem';
-import { ScrollView } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([]);
+    const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    function handleToggleFiltersVisible() {
+        setIsFiltersVisible(!isFiltersVisible);
+    }
+
+    async function handleFiltersSubmit() {
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setIsFiltersVisible(false);
+        setTeachers(response.data);
+    }
+
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys diponíveis" />
+            <PageHeader 
+                title="Proffys diponíveis"
+                headerRight={(
+                    <BorderlessButton onPress={handleToggleFiltersVisible}>
+                        <Feather name="filter" size={20} color="#FFF" />
+                    </BorderlessButton>
+                )}
+            >
+                { isFiltersVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}>Materia</Text>
+                        <TextInput 
+                            placeholderTextColor="#c1bccc" 
+                            style={styles.input} 
+                            value={subject}
+                            onChangeText={text => setSubject(text)}
+                            placeholder="Qual a matéria?"
+                        />
+
+                        <View style={styles.inputGroup}>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Dia da Semana</Text>
+                                <TextInput 
+                                    placeholderTextColor="#c1bccc" 
+                                    style={styles.input} 
+                                    value={week_day}
+                                    onChangeText={text => setWeekDay(text)}
+                                    placeholder="Qual o dia?"/>
+    
+                            </View>
+
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Horário</Text>
+                                <TextInput 
+                                    placeholderTextColor="#c1bccc" 
+                                    style={styles.input} 
+                                    value={time}
+                                    onChangeText={text => setTime(text)}
+                                    placeholder="Qual o horário?"
+                                />
+                            </View>
+                        </View>
+
+                        <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
+                            <Text style={styles.submitButtonText}>Filtrar</Text>
+                        </RectButton>
+                    </View>
+                )}
+            </PageHeader>
 
             <ScrollView 
                 style={styles.teacherList}
@@ -19,12 +93,9 @@ function TeacherList() {
                     paddingBottom: 16
                 }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher}/>
+                })}
 
             </ScrollView>
         </View>
